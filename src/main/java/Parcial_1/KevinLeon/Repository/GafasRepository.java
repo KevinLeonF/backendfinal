808141244db256cba3ddc69f3fb68c4a50a2057b
Kjleon <kj.leonflores@gmail.com>
2025-12-02 23:05:20 -0400

    feat: update the backend}

--- src/main/java/Parcial_1/KevinLeon/Repository/GafasRepository.java
+++
@@ -1,9 +0,0 @@
-package Parcial_1.KevinLeon.Repository;
-
-import Parcial_1.KevinLeon.Models.Gafas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GafasRepository extends JpaRepository<Gafas, Integer> {
-}
